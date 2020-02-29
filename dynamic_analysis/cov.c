cdb3eaa5a3d18dc60510682e1d1a064f59984be8
Bo Chen <bochen@Bos-MacBook-Pro.local>
2020-02-29 15:49:25 -0500

    modified cov.c

--- dynamic_analysis/cov.c
+++ dynamic_analysis/cov.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main (void)
+int main (int argc, char **argv)
 {
   int i;
 
-  for (i = 1; i < 10; i++)
+  for (i = atoi(argv[1]); i < 10; i++)
     {
       if (i % 3 == 0)
         printf ("%d is divisible by 3\n", i);
